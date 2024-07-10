a177117d7306d3adb58de67049cbcfc55a1ce6b0
JaeUk <siwall0105@gmail.com>
2024-07-11 02:56:50 +0900

    chore: Spring AOP 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
