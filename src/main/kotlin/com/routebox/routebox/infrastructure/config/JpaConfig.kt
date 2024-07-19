631f16e26f31b8aae4feb35b4f5f0e489febe938
JaeUk <siwall0105@gmail.com>
2024-07-19 21:48:06 +0900

    chore: #1 생성자(createdBy), 수정자(updatedBy)에 대한 JPA Auditing 설정

--- src/main/kotlin/com/routebox/routebox/infrastructure/config/JpaConfig.kt
+++ src/main/kotlin/com/routebox/routebox/infrastructure/config/JpaConfig.kt
@@ -1,16 +1,33 @@
 package com.routebox.routebox.infrastructure.config
 
+import com.routebox.routebox.domain.user.UserService
+import com.routebox.routebox.security.UserPrincipal
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.domain.AuditorAware
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContext
+import org.springframework.security.core.context.SecurityContextHolder
 import java.util.Optional
 
 @EnableJpaAuditing
 @Configuration
 class JpaConfig {
 
-    // TODO: 유저 entity 및 기능 구현 후 수정 필요
     @Bean
-    fun auditorAware(): AuditorAware<Long> = AuditorAware { Optional.of(1L) }
+    fun auditorAware(userService: UserService): AuditorAware<Long> = AuditorAware {
+        val principal = Optional.ofNullable(SecurityContextHolder.getContext())
+            .map(SecurityContext::getAuthentication)
+            .filter(Authentication::isAuthenticated)
+            .map(Authentication::getPrincipal)
+
+        if (principal.isEmpty || principal.get() == "anonymousUser") {
+            return@AuditorAware Optional.empty()
+        }
+
+        return@AuditorAware principal
+            .map { it as UserPrincipal }
+            .map { it.userId }
+    }
 }
