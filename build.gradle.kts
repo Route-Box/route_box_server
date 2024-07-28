ae8ff65b72e6bfa3a2f48396e2e5d5421a0ed7c1
JaeUk <siwall0105@gmail.com>
2024-07-28 19:33:08 +0900

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,7 +64,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 }
-
+//
 kotlin {
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
