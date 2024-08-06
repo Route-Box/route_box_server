623e6b248d7f15c8eb04183514bfc3443bdd19fc
suyeoniii <ssy4230@naver.com>
2024-08-07 04:09:31 +0900

    feat: #25 사용하지 않은 import 삭

--- src/main/kotlin/com/routebox/routebox/controller/route/RouteController.kt
+++ src/main/kotlin/com/routebox/routebox/controller/route/RouteController.kt
@@ -4,14 +4,11 @@ import com.routebox.routebox.application.route.GetLatestRoutesUseCase
 import com.routebox.routebox.application.route.GetRouteDetailUseCase
 import com.routebox.routebox.controller.route.dto.GetLatestRoutesResponse
 import com.routebox.routebox.controller.route.dto.RouteResponse
-import com.routebox.routebox.security.UserPrincipal
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
-import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
