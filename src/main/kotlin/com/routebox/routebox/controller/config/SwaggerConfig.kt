67dccc09414d5e545a8cf2f6e27efffd885a5ddf
suyeoniii <ssy4230@naver.com>
2024-08-08 20:08:10 +0900

    feat: #13 swagger server url 명시

--- src/main/kotlin/com/routebox/routebox/controller/config/SwaggerConfig.kt
+++ src/main/kotlin/com/routebox/routebox/controller/config/SwaggerConfig.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.ExternalDocumentation
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -14,6 +15,9 @@ class SwaggerConfig {
 
     @Value("\${routebox.app-version}")
     lateinit var appVersion: String
+
+    @Value("\${swagger.server-url}")
+    lateinit var serverUrl: String
 
     @Bean
     fun openApi(): OpenAPI =
@@ -36,4 +40,5 @@ class SwaggerConfig {
                         .bearerFormat("JWT"),
                 ),
             )
+            .servers(listOf(Server().url(serverUrl)))
 }
