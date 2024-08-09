90238b946cda48082b5fc66bef7287dc3405b955
suyeoniii <ssy4230@naver.com>
2024-08-09 16:30:01 +0900

    feat: #39 홈화면 - 추천, 인기 루트 mockup API

---
+++ src/main/kotlin/com/routebox/routebox/controller/route/dto/GetPopularRoutesResponse.kt
@@ -0,0 +1,14 @@
+package com.routebox.routebox.controller.route.dto
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+data class GetPopularRoutesResponse(
+    @Schema(description = "인기 루트 목록")
+    val routes: List<PopularRouteDto>,
+) {
+    companion object {
+        fun from(routes: List<PopularRouteDto>): GetPopularRoutesResponse = GetPopularRoutesResponse(
+            routes = routes,
+        )
+    }
+}
---
+++ src/main/kotlin/com/routebox/routebox/controller/route/dto/PopularRouteDto.kt
@@ -0,0 +1,18 @@
+package com.routebox.routebox.controller.route.dto
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+data class PopularRouteDto(
+    @Schema(description = "루트 ID")
+    val id: Long,
+
+    @Schema(description = "루트 이름")
+    val name: String,
+) {
+    companion object {
+        fun from(id: Long, name: String): PopularRouteDto = PopularRouteDto(
+            id = id,
+            name = name,
+        )
+    }
+}
