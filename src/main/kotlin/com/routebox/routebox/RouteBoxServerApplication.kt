983df7d2b2619ead09761ec77d8223cc5bbc5577
JaeUk <siwall0105@gmail.com>
2025-02-24 14:33:09 +0900

    chore: page serialization mode 설정 추가

--- src/main/kotlin/com/routebox/routebox/RouteBoxServerApplication.kt
+++ src/main/kotlin/com/routebox/routebox/RouteBoxServerApplication.kt
@@ -4,8 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.PropertySource
+import org.springframework.data.web.config.EnableSpringDataWebSupport
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode
 import org.springframework.retry.annotation.EnableRetry
 
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 @EnableRetry
 @ConfigurationPropertiesScan
 @PropertySource("classpath:/env.properties")
